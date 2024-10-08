dc7778f2bc14cac6d6665387df324d33d2922b33
Youssef Ababou <3b.youssef@mail.com>
2024-10-08 12:55:25 +0100

    Structure of the web site

---
+++ src/components/Layout.jsx
@@ -0,0 +1,43 @@
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+  return (
+    <>
+      <section className="bg-white h-96 px-64">
+        <nav className="flex justify-between items-center py-6">
+          <h2 className="font-bold text-xl text-sky-600">NEXTSTAVIA</h2>
+          <ul className="flex space-x-8">
+            <li className="font-semibold cursor-pointer hover:text-sky-400 transition-all">
+              Home
+            </li>
+            <li className="font-semibold cursor-pointer hover:text-sky-500 transition-all">
+              Doctors
+            </li>
+            <li className="font-semibold cursor-pointer hover:text-sky-500 transition-all">
+              Patients
+            </li>
+            <li className="font-semibold cursor-pointer hover:text-sky-500 transition-all">
+              Pharmacy
+            </li>
+            <li className="font-semibold cursor-pointer hover:text-sky-500 transition-all">
+              Page
+            </li>
+            <li className="font-semibold cursor-pointer hover:text-sky-500 transition-all">
+              Admin
+            </li>
+          </ul>
+          <div className="flex space-x-3">
+            <button className="bg-white px-3 py-1 border rounded-md text-sm font-semibold hover:bg-sky-500 hover:border hover:border-sky-500 hover:text-white transition-all">
+              Register
+            </button>
+            <button className="bg-sky-500 px-3 py-1 border rounded-md text-sm font-semibold border-sky-500 text-white hover:border-white hover:bg-white hover:text-black transition-all ">
+              Login
+            </button>
+          </div>
+        </nav>
+        <Outlet />
+      </section>
+    </>
+  );
+}
+export default Layout;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function Home() {
+  return <div>Home</div>;
+}
+export default Home;
